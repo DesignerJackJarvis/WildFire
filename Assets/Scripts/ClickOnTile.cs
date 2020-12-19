2e42a150b644a6325b7b2f57ffc4b702d1661630
Jackoberto <jackoberto01@gmail.com>
2020-12-19 13:40:40 +0100

    started working on instantiation on tile map

---
+++ Assets/Scripts/ClickOnTile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class ClickOnTile : MonoBehaviour
+{
+    public GameObject whatToInstantiate;
+    public Grid grid;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            var position = grid.WorldToCell(worldPoint);
+            var instantiate = Instantiate(whatToInstantiate);
+            instantiate.transform.position = grid.CellToWorld(position);
+        }
+    }
+}
