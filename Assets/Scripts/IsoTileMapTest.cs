ed338b1ad88b7cf599fdcc806cbb7136572e4ee4
Jackoberto <jackoberto01@gmail.com>
2020-12-19 13:28:17 +0100

    starts on code

---
+++ Assets/Scripts/IsoTileMapTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class IsoTileMapTest : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public Tile tile;
+    public Vector3Int position;
+
+    private void Start()
+    {
+        Debug.Log(tileMap.size);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tileMap.SetTile(position, tile);
+        }
+    }
+}
