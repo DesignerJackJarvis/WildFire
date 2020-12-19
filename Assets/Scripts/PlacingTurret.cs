056fbbc119649844fc2d2a667fe80492d2103219
Jackoberto <jackoberto01@gmail.com>
2020-12-19 20:24:31 +0100

    Adds Tree Tiling and Turret Placing

--- Assets/Scripts/PlacingTurret.cs
+++ Assets/Scripts/PlacingTurret.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlacingTurret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public NotPlaceAble notPlaceableTiles;
+    public TileBase turretTile;
+    public GameObject objectToPlace;
+    public Grid grid;
+    public Tilemap tilemap;
+    public bool placeTurretMode = true;
 
-    // Update is called once per frame
-    void Update()
+    private bool IsAvailable(TileBase tileBase1, TileBase tileBase2)
     {
-        
+        if (notPlaceableTiles.tiles.Any(tile => tile == tileBase1 || tile == tileBase2))
+        {
+            return false;
+        }
+
+        return tileBase1 != null;
+    }
+    
+    private void Update()
+    {
+        if (placeTurretMode && Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            var position = grid.WorldToCell(worldPoint);
+            if (IsAvailable(tilemap.GetTile(position), SpreadingFire.FireTilemap.GetTile(position)))
+            {
+                var instantiate = Instantiate(objectToPlace);
+                var cellToWorld = grid.CellToWorld(position);
+                SpreadingFire.FireTilemap.SetTile(position, turretTile);
+                instantiate.transform.position = cellToWorld + new Vector3(0.5f, 0.5f); 
+            }
+        }
     }
 }
