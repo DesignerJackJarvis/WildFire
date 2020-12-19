881ea8d142c1cfede6140162a9cf5b1b7b91271e
Jackoberto <jackoberto01@gmail.com>
2020-12-19 23:52:13 +0100

    Fixes TreeGeneration

---
+++ Assets/Scripts/TreeGeneration.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TreeGeneration : MonoBehaviour
+{
+    public NotPlaceAble spawnAbleTiles;
+    public int grassToTreeRatio;
+    public GameObject tree;
+    private int _spawnAbleCount;
+    private List<Vector3Int> _spawnAblePlaces = new List<Vector3Int>();
+    private List<Vector3Int> _alreadySpawnedPlaces = new List<Vector3Int>();
+    private Tilemap _tilemap;
+
+    bool SpawnAble(Vector3Int position)
+    {
+        if (_alreadySpawnedPlaces.Count == 0)
+            return true;
+        foreach (var alreadySpawnedPlace in _alreadySpawnedPlaces)
+        {
+            if (Vector3Int.Distance(alreadySpawnedPlace, position) < Mathf.Abs(1.5f))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private void Start()
+    {
+        _tilemap = PlacingTurret.tilemap;
+        for (var i = 0; i < _tilemap.size.x; i++)
+        {
+            for (var e = 0; e < _tilemap.size.y; e++) 
+            {
+                var tile = _tilemap.GetTile(new Vector3Int(_tilemap.origin.x + i, _tilemap.origin.y + e, 0));
+                if (tile == null || tile != spawnAbleTiles.tiles[0] && tile != spawnAbleTiles.tiles[1]) continue;
+                Debug.Log(_spawnAbleCount);
+                _spawnAbleCount++;
+                _spawnAblePlaces.Add(new Vector3Int(_tilemap.origin.x + i, _tilemap.origin.y + e, 0));
+            }
+        }
+
+        for (var i = 0; i < _spawnAbleCount / grassToTreeRatio; i++)
+        {
+            var newTree = Instantiate(tree, transform);
+            var position = new Vector3Int();
+            do
+            {
+                position = _spawnAblePlaces[Random.Range(0, _spawnAblePlaces.Count)];
+            } while (!SpawnAble(position));
+            
+            newTree.transform.position = position + new Vector3(0.5f, 0.5f, 0);
+            _alreadySpawnedPlaces.Add(position);
+        }
+    }
+}
--- Assets/Scripts/TreeTile.cs
+++ Assets/Scripts/TreeTile.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TreeTile : MonoBehaviour, IDamageAble
 {
     public Tile tile;
     public float health = 50;
+    private SpriteRenderer _spriteRenderer;
+    private Color _originalColor;
+    private float _time;
     public float Health
     {
         get => health;
         set
         {
             health = value;
+            _time = 1;
             if (health <= 0)
             {
                 //var location = SpreadingFire.FireTilemap.WorldToCell(transform.position - new Vector3(0.5f, 0.5f, 0));
@@ -23,6 +28,16 @@ public class TreeTile : MonoBehaviour, IDamageAble
     {
         var position = FindObjectOfType<Grid>().WorldToCell(transform.position);
         SpreadingFire.FireTilemap.SetTile(position, tile);
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _originalColor = _spriteRenderer.color;
+    }
+
+    private void Update()
+    {
+        _spriteRenderer.color = Color.Lerp(_originalColor, Color.red, _time);
+        {
+            _time -= Time.deltaTime;
+        }
     }
 
     public bool TakeDamage(float damage)
